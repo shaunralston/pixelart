857822117065b558d370e43d82724b88c26257c0
Shaun Ralston <44296095+shaunralston@users.noreply.github.com>
2018-11-30 06:06:24 -0800

    Pixel Art Maker, JS Project for Udacity
    Submitted project files

--- designs.js
+++ designs.js
@@ -1,36 +1,29 @@
-// Select color input
-// Select size input
-var height, width, color;
+var cols, rows, color; //variables define grid size and color
 
-// When size is submitted by the user, call makeGrid()
-$('#sizePicker').submit(function (event) {
+$('#sizePicker').submit(function (event) { //creates the grid with input via the submit button
   event.preventDefault();
-  height = $('#inputHeight').val();
-  width = $('#inputWidth').val();
-  makeGrid(height, width);
+  cols = $('#inputHeight').val();
+  rows = $('#inputWidth').val();
+  makeGrid(cols, rows);
 })
 
-// call the makeGrid function - height and width
-function makeGrid(h, w) {
-  $('tr').remove();
+function makeGrid(c, r) { //call the makeGrid function, pass the height and width
+  $('tr').remove(); //clears the grid when user hits submit again
 
-
-for (var i = 1; i <= h; i++) {
-  $('#pixelCanvas').append('<tr id=table' + i + '></tr>');
-  for (var j = 1; j <= w; j++) {
-    $('#table' + i).append('<td></td>');
+for (var i = 1; i <= c; i++) {
+  $('#pixelCanvas').append('<tr id=table' + i + '></tr>'); // this loop adds a row of cells
+  for (var j = 1; j <= r; j++) {
+    $('#table' + i).append('<td></td>'); // this adds a cell after each row
   }
 }
 
-// add color to clicked cell
-// remove color from clicked cell
-$('td').click(function addColor() {
+$('td').click(function addColor() { // allows user to color each cell with a click
   color = $('#colorPicker').val();
 
   if ($(this).attr('style')) {
-    $(this).removeAttr('style')
+    $(this).removeAttr('style') // returns the color
   } else {
-    $(this).attr('style', 'background-color:' + color);
+    $(this).attr('style', 'background-color:' + color); // lets the color be returned to background
 
   }
 })
